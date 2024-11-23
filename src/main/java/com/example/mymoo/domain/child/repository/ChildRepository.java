366c2a86de57849c5e6bced08845752986853b1e
hyeneung <imasaru22@gmail.com>
2024-11-23 23:31:00 +0900

    #21 - fix: security config and child domain's conflict

--- src/main/java/com/example/mymoo/domain/child/repository/ChildRepository.java
+++ src/main/java/com/example/mymoo/domain/child/repository/ChildRepository.java
@@ -1,8 +1,10 @@
 package com.example.mymoo.domain.child.repository;
 
 import com.example.mymoo.domain.child.entity.Child;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
     boolean existsByAccount_Id(Long accountId);
+    Optional<Child> findByAccount_Id(Long accountId);
 }
