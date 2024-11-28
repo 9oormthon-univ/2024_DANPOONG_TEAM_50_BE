a90df1765ef73ad7303132a5e4e3a279ed87e9dd
hyeneung <imasaru22@gmail.com>
2024-11-28 14:15:47 +0900

    #33 - feat: Add custom exception for unauthorized message update
    - Implement exception to throw when validating child's authority to write a message

--- src/main/java/com/example/mymoo/domain/donationusage/exception/DonationUsageExceptionDetails.java
+++ src/main/java/com/example/mymoo/domain/donationusage/exception/DonationUsageExceptionDetails.java
@@ -12,6 +12,8 @@ public enum DonationUsageExceptionDetails implements ExceptionDetails {
     DONATION_USAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 donationUsage 입니다."),
     // 자신의 가게가 아닌 다른 가게의 후원을 사용하려 할 때
     FORBIDDEN_ACCESS_TO_OTHER_STORE(HttpStatus.FORBIDDEN, "해당 후원을 사용할 권한이 없습니다."),
+    // 자신이 사용하지 않은 후원권에 대해 후원자에게 감사 메시지를 쓰려 할 때
+    FORBIDDEN_TO_WRITE_MESSAGE_TO_OTHER_DONATION(HttpStatus.FORBIDDEN, "해당 후원에 대해 메시지를 작성할 권한이 없습니다."),
     ;
 
     private final HttpStatus status;
