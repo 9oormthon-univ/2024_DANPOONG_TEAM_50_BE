75b65649e3f017b93b5c24f4d637ab401c1fb354
hyeneung <imasaru22@gmail.com>
2024-11-28 14:17:48 +0900

    #33 - feat: Implement server-side logic to handle update message api

--- src/main/java/com/example/mymoo/domain/donationusage/service/DonationUsageService.java
+++ src/main/java/com/example/mymoo/domain/donationusage/service/DonationUsageService.java
@@ -1,10 +1,16 @@
 package com.example.mymoo.domain.donationusage.service;
 
 import com.example.mymoo.domain.donationusage.dto.request.DonationUsageCreateRequestDto;
+import com.example.mymoo.domain.donationusage.dto.request.DonationUsageUpdateMessageRequestDto;
 
 public interface DonationUsageService {
     void useDonation(
         Long storeAccountId,
         DonationUsageCreateRequestDto donationUsageCreateRequestDto
+    );
+
+    void updateMessage(
+        Long childAccountId,
+        DonationUsageUpdateMessageRequestDto donationUsageUpdateMessageRequestDto
     );
 }
